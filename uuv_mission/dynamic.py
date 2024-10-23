95a6e057c40ea23b04c6274929c296489bfc6771
Xiaoyi Shang <xiaoyi.shang@jesus.ox.ac.uk>
2024-10-23 23:07:19 +0100

    Complete ClosedLoop Class

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -97,8 +97,11 @@ class ClosedLoop:
         for t in range(T):
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
-            # Call your controller here
+            # Call the controller to compute the control action
+            actions[t] = self.controller.compute_control_action(mission.reference[t], observation_t)
+            # Update the submarine's state with the control action and disturbance
             self.plant.transition(actions[t], disturbances[t])
+    
 
         return Trajectory(positions)
         
